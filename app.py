ebd4f8cf42e3f22b95a1250b2e79b2a56d738756
Felix-Nabiswa <felix.nabiswa@kibo.school>
2023-03-11 08:09:20 +0300

    css _added

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from quizes import display_questions
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder= "static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.secret_key = 'mysecretkey'
 db = SQLAlchemy(app)
 
+# User Model
 class Gamer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -18,14 +19,31 @@ class Gamer(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+    # Relationship to QuizScore Model
+    quiz_scores = db.relationship('QuizScore', backref='gamer', lazy=True)
 
+
+# QuizScore Model
+class QuizScore(db.Model):
+    __tablename__ = 'quiz_scores'
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+
+    # Foreign Key to User Model
+    user_id = db.Column(db.Integer, db.ForeignKey('gamer.id'), nullable=False)
+
+# Create all the tables
 with app.app_context():
     db.create_all()
 
 
 @app.route("/")
 def index():
+    if 'logged_in' in session and session['logged_in']:
+        return redirect(url_for('questions'))
     return render_template("home.html")
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -47,6 +65,7 @@ def register():
         user = Gamer(username=username, email=email, password=password)
         db.session.add(user)
         db.session.commit()
+        
         return redirect(url_for('login'))
 
     return render_template('register.html')
@@ -65,10 +84,21 @@ def login():
             # set the user as logged in
             session['user_id'] = user.id
             session['logged_in'] = True
-            # redirect to the home page
-            return redirect(url_for('index'))
+            # redirect to the questions page
+            return redirect(url_for('questions'))
+
     # render the login page
     return render_template('login.html', error=error)
+
+@app.route("/questions")
+def questions():
+    # Check if user is logged in
+    if 'logged_in' not in session or not session['logged_in']:
+        return redirect(url_for('login'))
+
+    # Render the questions page
+    return render_template("questions.html", display_questions=display_questions)
+
 
 @app.route("/results", methods=["POST"])
 def results():
@@ -77,4 +107,19 @@ def results():
         answered = request.form.get(question["question"])
         if answered == question["answer"]:
             score += 1
+
+    # Create a new QuizScore object and add it to the database
+    quiz_score = QuizScore(score=score, user_id=session['user_id'])
+    db.session.add(quiz_score)
+    db.session.commit()
+
     return render_template("results.html", score=score, display_questions=display_questions)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', None)
+    session.pop('logged_in', False)
+    session.clear()
+    return redirect(url_for('index'))
+
+
